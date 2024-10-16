c774a38e8c5fb903c3e340b4087a6b31d2b46194
abdullah almohamadi <149333381+4li9@users.noreply.github.com>
2024-10-16 18:23:58 +0300

    d

---
+++ lib/aa.dart
@@ -0,0 +1,11 @@
+ /*تنيل المشروع
+git clone https://github.com/4li9/Travel-app.git
+
+//لارسال التعديلات
+git add .
+git commit -m "كتابه التعديلات هنا"
+git push
+
+//سحب اي تعديلات جديده 
+git pull
+*/
